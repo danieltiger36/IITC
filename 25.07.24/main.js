fe1b94aa1661999cda10277bbde6fe79c5e0a964
danieltiger36 <danieltigar36@gmail.com>
2024-07-25 16:27:39 +0300

    init repo

---
+++ 25.07.24/main.js
@@ -0,0 +1,16 @@
+
+
+let a = 5;
+let b = 10;
+console.log (a);
+console.log (a + b);
+let level = 1
+console.log (level);
+function levelUp () {
+ level++;
+ console.log (level);
+}
+function greet () {
+    console.log ('hello world');
+}
+ greet ();
---
+++ hw task1/main.js
@@ -0,0 +1,24 @@
+console.log('hello world')
+///task1
+let firstName = "daniel";
+///task2
+let age = 20;
+///task3
+let isRaining = false;
+///task4
+const Pi = 3.14159;
+///task5
+let isStudent = true;
+///task6
+let favoriteColor;
+///task7
+let temperature = 72.5;
+///task8
+const DAYS_IN_WEEK = 7;
+///task9
+let username = 'coder123';
+///task10
+let price = 0;
+///task11
+ let isLoggedIn = false;
+
