43d61cecf17a8535fa0c9d6267cb924c2a440a3d
danieltiger36 <danieltigar36@gmail.com>
2024-07-26 20:05:10 +0300

    commit

--- hw task1/main.js
+++ hw task1/main.js
@@ -18,7 +18,28 @@ const DAYS_IN_WEEK = 7;
 ///task9
 let username = 'coder123';
 ///task10
-let price = 0;
+let hits = 0;
 ///task11
  let isLoggedIn = false;
+ ///task12
+ const COMPANY_NAME = "Tech Solutions Inc";
+ ///task13
+let currentYear = new Date() .getFullYear();
+///task14
+let price = 19.99; 
+///task15
+let quantity = 100;
+///task16
+let counter = 0;
+let increment = counter++;
+///task17
+let message = "Hello";
+message = "Hello, World!";
+///task18
+
+
+
+
+
+
 
