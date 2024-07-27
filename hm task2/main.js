97cc90028c2167245d54176c651b073521bc095f
danieltiger36 <danieltigar36@gmail.com>
2024-07-27 15:45:22 +0300

    hw task2

---
+++ hm task2/main.js
@@ -0,0 +1,51 @@
+console.log("hello world");
+///task1
+//answear: number
+///task2
+let string = "string";
+///task3
+///null is assigned value that mean nothing, undefined is not defiined
+let uf;
+let nu = null;
+///task4
+console.log(typeof string);
+///task5
+///float
+console.log(5/2);
+///task6
+///boolean true or falses
+///task7
+const me = {firstName:"dniel", age: 20, eyeColor:"green"};
+///task8
+///boolean true or fales
+///task9
+/// == check value, === chech value and type
+///task10
+let cash = "15";
+Number(cash);
+///task11
+///number javascript convert automaticlly string to number in mathematical operations
+///task12
+typeof new Object();//you can do also array, date....
+///task13
+let mls = 'apple\
+banna\
+orange';
+///task14
+/// the difference beween singel and double quotes is in single quetes you can put double quotes in the values without an error
+///and the backticks can spam multiple lines incloud variable and special characters
+///task15
+let meters = 3;
+toString(meters);
+///task16
+
+
+
+
+
+
+
+
+
+
+
