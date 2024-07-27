e81ea54ba8713c39db1e8b7a2110a699c08ff85c
danieltiger36 <danieltigar36@gmail.com>
2024-07-27 19:36:39 +0300

    hm  topic3 finished

---
+++ hm task3 compartion operators/main.js
@@ -0,0 +1,33 @@
+console.log("hello world")
+///task1
+///equal or not value and type or greater or less then 
+///task2
+/// == for value === for value and type
+///task3
+///!= not equal value !== not equal value and type
+///task4
+///comparing if the number is greater then
+///task5
+///comparing if the number is less then
+///task6
+///by <= 
+///task7
+///by <=
+///task8
+/// true 
+///task9
+/// false the value is same but the type is diffrent 
+///task10
+///true because its less or equal 
+///task11
+///same opartiors as numbers but the value is in alphabetical order
+///task12
+///true
+///task13
+///not equal to value and data type
+///task14
+///true the type is different
+///task15
+///its can convert string to number data type automatically
+
+
