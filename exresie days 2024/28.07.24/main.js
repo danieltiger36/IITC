71cea81bd253e4d4ccb3c27f0a5726a8ee978723
danieltiger36 <danieltigar36@gmail.com>
2024-07-28 12:29:39 +0300

    class practice

---
+++ exresie days 2024/28.07.24/main.js
@@ -0,0 +1,25 @@
+console.log("hello")
+///for loop
+//for (let i = 0; i < 10; i++) {
+//    console.log(`the current loop iteration is: ${i}`);
+//}
+///practice task1
+//for (let i = 10; i > 0; i--) {
+//    console.log(`the current loop iteration is: ${i}`);
+//}
+///pratice task2
+//for (let i = 0; i < 20; i+=2) {
+//    console.log(`the current loop iteration is ${i}`)
+//}
+///practice task3
+//for (let i = 0; i <= 10; i+=1) {
+//    console.log(`the current loop iteration is ${i}`)
+//}
+///practice task4
+//for (i=5; i <=5-; i +=5){
+//    console.log(i);
+//}
+///task5
+///for(i = 0; i < 6; i ++){
+//    console.log("*".repeat (i));
+//}
