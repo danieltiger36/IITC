1fb3c14af7dec83b561520aa6ec489a328833b5b
danieltiger36 <danieltigar36@gmail.com>
2024-08-01 10:18:23 +0300

    inclass

---
+++ 01.08.24/main.js
@@ -0,0 +1,11 @@
+console.log("morni");
+//multipliction table
+for (let i = 1; i <=10; i++){
+    let output = ``;
+    for (let j = 1; j <= 10; j++) {
+     output += `${j*i} `
+
+    }
+
+    console.log(output);
+}
