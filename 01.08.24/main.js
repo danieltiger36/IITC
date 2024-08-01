5a290e781de14efba5c2e666c3f687fbfd80f2d3
danieltiger36 <danieltigar36@gmail.com>
2024-08-01 15:38:57 +0300

    inclass

--- 01.08.24/main.js
+++ 01.08.24/main.js
@@ -10,3 +10,59 @@
 //     console.log(output);
 // }
 ///////////////////////
+//e1
+let person = {
+    name:"daniel",
+    age:20,
+    isStudent:true
+}
+console.log(person.name, person.age);
+person.isStudent = false;
+console.log(person);
+//e2
+let car = {
+    make:"fiat",
+    model:"500",
+    year:2020
+}
+console.log(car.make , car.model);
+car.year = 2024
+console.log(car);
+let fruit = {
+    name:"mango",
+    color:"yellow",
+    sweetnes:7
+}
+console.log(fruit.name, fruit.sweetnes);
+fruit.color = "green";
+console.log(fruit);
+//e5
+let animel = {
+species:"cow",
+sound:"moo",
+isWild:true
+}
+////////////////////////
+//easy nested loops
+//e1
+// for (i = 0; i < 2; i++) {
+//     for (j = 0; j < 2; j++);{
+    
+//         console.log(`**`);
+//     }
+// console.log(`**`);
+// }
+/////e2
+// for (i = 1; i <= 1; i++) {
+//     for (j = 1; j <= 4; j++);{
+    
+// console.log(1,2);
+       
+// }
+// console.log(3,4);
+//}
+//e3
+
+
+
+
