355e7aede116888e87bd0d2beefedaac08b837c1
danieltiger36 <danieltigar36@gmail.com>
2024-08-05 15:40:47 +0300

    inclass

---
+++ 05.08/main.js
@@ -0,0 +1,43 @@
+//e1
+// for (let i = 0; i <2; i++){
+//     let row = " ";
+//     for (j = 0; j <2; j++){
+//     (row += "*")
+// }
+// console.log(row);
+// }
+
+//e2
+// for (let i = 0; i < 2; i++) {
+//     let counter = 0;
+//     let row = ""
+//     for (let j = 1; j <= 4; j++){
+//         counter += 1;
+//         row += counter;
+        
+//     }
+//     console.log(row);
+// }
+
+//e3
+// for (let i = 0; i < 1; i++) {
+//     let counter = 0;
+//     let stars = " ";
+//     for (let j = 0; j <3; j++){
+//      stars += "*"
+//      console.log(stars);
+//     }
+    
+// }
+//e4
+
+for (let i = 1; i<4; i++){
+    let row = ""
+    let count = 0;
+    for (let j = 1; j <4; j++){
+    row += i*j + " ";
+    count += j*i;
+    
+    }
+    console.log(row,"total sum", count);
+}
